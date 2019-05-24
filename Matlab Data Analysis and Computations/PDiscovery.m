b1f6ad8d5b23ad43a80ae056479af9781fce9494
CollinBrady1993 <36489775+CollinBrady1993@users.noreply.github.com>
2019-05-24 16:02:31 -0700

    Update PDiscovery.m

--- Matlab Data Analysis and Computations/PDiscovery.m
+++ Matlab Data Analysis and Computations/PDiscovery.m
@@ -23,7 +23,7 @@ for r = 1:min(Nr,Nut+Npt)
         %number of X that become the given A vector
         num = (factorial(Nr)/(factorial(r)*factorial(Nr-r)))*factorial(Nut)/prod(factorial(AR(i,:)))*factorial(Npt)/prod(factorial(AI(i,:)))*(factorial(r)/(factorial(length(AR(i,AR(i,:)>0)))*factorial(r - length(AR(i,AR(i,:)>0)))))*(factorial(length(AR(i,AR(i,:)>0)))/prod(factorial(JR(i,:))))*numWaysToDistributeDiscUsers(AR(i,:),AI(i,:),Nut+Npt);
         %number of discoveries
-        k = sum(AR(i,:)==1);
+        k = sum(and(AR(i,:)==1,AI(i,:)==0));
         
         fk(k+1) = fk(k+1) + num*prob;
         
